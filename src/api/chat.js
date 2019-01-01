3c6aee00d5a08560e9b877d9192b66ae8df4517f
szeseong.lim <lseong@gmail.com>
2019-01-01 18:13:17 +0800

    added jest for testing

--- src/api/chat.js
+++ src/api/chat.js
@@ -1,25 +1,24 @@
-const axios = require("axios")
+const axios = require("axios");
 
-const token = process.env.API_TOKEN
-const url = "https://slack.com/api/chat.postMessage"
+const token = process.env.API_TOKEN;
+const url = "https://slack.com/api/chat.postMessage";
 
 function channelPostMessage(channel) {
-	return async message => {
-		const response = await axios({
-			method: "post",
-			url: url,
-			headers: {
-				"content-type": "application/json;charset=utf-8",
-				Authorization: `Bearer ${token}`,
-			},
-			data: {
-				channel,
-				text: message,
-			},
-		})
-
-		return response.ok
-	}
+  return async message => {
+    const response = await axios({
+      method: "post",
+      url: url,
+      headers: {
+        "content-type": "application/json;charset=utf-8",
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        channel,
+        text: message,
+      },
+    });
+    return response.data.ok;
+  };
 }
 
-module.exports = {channelPostMessage}
+module.exports = { channelPostMessage };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-const {channelPostMessage} = require("./api/chat")
+const { channelPostMessage } = require("./api/chat");
 
-const message = process.argv[2]
+const message = process.argv[2];
 if (message) {
-	// channel("#general").postMessage(message);
-	// channelPostMessage("#general")(message);
-	const postGeneralMessage = channelPostMessage("#general")
-	const responseOk = postGeneralMessage(message)
-	if (responseOk) {
-		console.log("Message sent ✔️ ")
-	}
+  const postGeneralMessage = channelPostMessage("#general");
+  postGeneralMessage(message).then(responseOk => {
+    if (responseOk === true) {
+      console.log("Message sent ✔️ ");
+    }
+  });
 }
